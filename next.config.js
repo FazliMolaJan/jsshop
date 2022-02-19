154d696fea41cd22ac7dcade23bc48ebdff8fe1e
jsstackdevelopers <javascriptstack0011@gmail.com>
2022-02-20 03:21:29 +0500

    env-fix

--- next.config.js
+++ next.config.js
@@ -8,4 +8,10 @@ module.exports = {
       "designcode.io",
     ],
   },
+  env: {
+    GOOGLE_CLIENT_ID: '151577407451-lk9a1timb36jst91ivkoufhm052n8h7c.apps.googleusercontent.com',
+    GOOGLE_CLIENT_SECRET: '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',
+    NEXTAUTH_URL: 'http://localhost:3000',
+    MONGODB_URI: 'mongodb+srv://jsstackdevelopers:root0011@clustor0.pxzd1.mongodb.net/jsshopDB?retryWrites=true&w=majority'
+  }
 };
